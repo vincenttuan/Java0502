fa37068e06f9093878a9c104d142cf1b9d376533
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 16:13:07 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day02/ArraysDemo4.java
@@ -0,0 +1,9 @@
+package com.lab.ocp.day02;
+
+public class ArraysDemo4 {
+    public static void main(String[] args) {
+        int[] scores = {40, 70, 30, 90};
+        // 請計算出及格分數之平均
+        
+    }
+}

dc94c82e2f16b508624e637efd997c14ea48b4c4
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 16:24:20 +0800

    Java

--- src/main/java/com/lab/ocp/day02/ArrayDemo4.java
+++ src/main/java/com/lab/ocp/day02/ArrayDemo4.java
@@ -3,7 +3,18 @@ package com.lab.ocp.day02;
 public class ArrayDemo4 {
     public static void main(String[] args) {
         int[] scores = {40, 70, 30, 90, 100};
-        // 請計算出及格分數之平均
-        
+        // 請計算出及格分數之平均(小數點一位)
+        // Java 7
+        int count = 0; // 有幾個及格分數(當分母用)
+        int sum = 0;  // 及格分數的總和(當分子用)
+        for(int i=0;i<scores.length;i++) {
+            if(scores[i] >= 60) {
+                count += 1;
+                sum += scores[i];
+            }
+        }
+        System.out.printf("count= %d sum= %d\n", count, sum);
+        System.out.printf("%.1f", (double)sum/count);
+        //----------------------------------------------------
     }
 }

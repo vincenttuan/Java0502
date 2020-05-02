ffe50f81cfff3bc892035f308c3cd917c6325cdb
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 16:14:32 +0800

    Java

--- src/main/java/com/lab/ocp/day02/ArrayDemo4.java
+++ src/main/java/com/lab/ocp/day02/ArrayDemo4.java
@@ -1,6 +1,6 @@
 package com.lab.ocp.day02;
 
-public class ArraysDemo4 {
+public class ArrayDemo4 {
     public static void main(String[] args) {
         int[] scores = {40, 70, 30, 90, 100};
         // 請計算出及格分數之平均

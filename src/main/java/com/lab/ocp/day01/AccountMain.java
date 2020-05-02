1a17712ad90a5b9bf73f7288e017449d9a79619b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 11:01:54 +0800

    Java

--- src/main/java/com/lab/ocp/day01/AccountMain.java
+++ src/main/java/com/lab/ocp/day01/AccountMain.java
@@ -5,7 +5,9 @@ public class AccountMain {
     public static void main(String[] args) {
         Account acct = new Account();
         acct.name = "Vincent";
-        acct.balance = 100000;
+        acct.deposit(10_0000);
+        acct.deposit(30000);
+        acct.deposit(-30000);
         acct.print();
     }
 }

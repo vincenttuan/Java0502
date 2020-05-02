17a183de362f11fd400cf0b83986c58974c0a4a9
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 10:55:16 +0800

    Java

--- src/main/java/com/lab/ocp/day01/AccountMain.java
+++ src/main/java/com/lab/ocp/day01/AccountMain.java
@@ -3,6 +3,9 @@ package com.lab.ocp.day01;
 // ATM 主程式
 public class AccountMain {
     public static void main(String[] args) {
-        
+        Account acct = new Account();
+        acct.name = "Vincent";
+        acct.balance = 100000;
+        acct.print();
     }
 }

af94043ff82f21fa4dd324643962be57682c58d3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 10:22:53 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day01/Circle.java
@@ -0,0 +1,12 @@
+package com.lab.ocp.day01;
+
+public class Circle {
+    String name; // 名字
+    int r; // 半徑
+    double area; // 面積
+    void print() { // 印出資料
+        System.out.println(name);
+        System.out.println(r);
+        System.out.println(area);
+    }
+}

00407dbad94cee4ad1643dc11d4024f498751e72
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 10:59:55 +0800

    Java

--- src/main/java/com/lab/ocp/day01/Account.java
+++ src/main/java/com/lab/ocp/day01/Account.java
@@ -2,7 +2,15 @@ package com.lab.ocp.day01;
 
 public class Account {
     String name; // 帳戶名
-    int balance; // 帳戶餘額
+    private int balance; // 帳戶餘額
+    
+    void deposit(int money) { // 存款
+        if(money > 0) {
+            balance += money;
+        } else {
+            System.out.printf("存款金額錯誤: %d\n", money);
+        }
+    }
     
     void print() { // 列印帳戶資訊
         System.out.printf("%s 帳戶餘額: $%,d\n", name, balance);

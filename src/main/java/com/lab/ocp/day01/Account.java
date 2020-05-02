ef896948cb1b72d9be92fc4318cf1a4b1bb7c1a1
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 11:36:51 +0800

    Java

--- src/main/java/com/lab/ocp/day01/Account.java
+++ src/main/java/com/lab/ocp/day01/Account.java
@@ -12,6 +12,19 @@ public class Account {
         }
     }
     
+    void withdraw(int money) { // 提款
+        if(money > 0) {
+            if(balance >= money) {
+                balance -= money;
+            } else {
+                System.out.println("帳戶餘額不足");
+                print(); // 印出帳戶資料
+            }
+        } else {
+            System.out.printf("提款金額錯誤: %d\n", money);
+        }
+    }
+    
     void print() { // 列印帳戶資訊
         System.out.printf("%s 帳戶餘額: $%,d\n", name, balance);
     }

0b8964a54622331f8583af8e5b66e1042b1d9d57
MB-teacher <MB-teacher@192.168.1.109>
2020-05-02 14:14:48 +0800

    Java

--- src/main/java/com/lab/ocp/day02/PhoneMain.java
+++ src/main/java/com/lab/ocp/day02/PhoneMain.java
@@ -1,7 +1,9 @@
 package com.lab.ocp.day02;
 
+import com.lab.ocp.day02.bar.CellPhone;
+
 public class PhoneMain {
     public static void main(String[] args) {
-        
+        CellPhone cp = new CellPhone();
     }
 }

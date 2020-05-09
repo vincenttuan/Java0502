15a715f3330409b0ea5f14c9840b038c9a3a2bff
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 15:35:26 +0800

    Java

--- src/main/java/com/lab/ocp/day03/TicGame2.java
+++ src/main/java/com/lab/ocp/day03/TicGame2.java
@@ -1,22 +1,12 @@
 package com.lab.ocp.day03;
 
 // 井字遊戲對戰
-
-import java.util.Scanner;
-
-public class TicGame {
+public class TicGame2 {
     public static void main(String[] args) {
         char[] c = new char[9];
-        do {            
-            Scanner sc = new Scanner(System.in);
-            System.out.println("請選擇位置(0~8): ");
-            int pos = sc.nextInt();
-            c[pos] = 'O';
-            print(c);
-            winner(c);
-        } while (true);
-        
-        
+        c[0]='O';c[2]='X';c[3]='X';c[4]='O';c[8]='O';
+        print(c);
+        winner(c);
     }
     
     public static void print(char [] c) {
@@ -24,9 +14,6 @@ public class TicGame {
             System.out.print(c[i] + "\t");
             if(i % 3 == 2) {
                 System.out.println();
-                if(i != 8) {
-                    System.out.println("------------------");
-                }
             }
         }
     }
@@ -41,7 +28,6 @@ public class TicGame {
            c[0] + c[4] + c[8] == 'O' * 3 || 
            c[2] + c[4] + c[6] == 'O' * 3 ) {
             System.out.println("Winner: O");
-            System.exit(0);
         } else if(c[0] + c[1] + c[2] == 'X' * 3 || 
            c[3] + c[4] + c[5] == 'X' * 3 || 
            c[6] + c[7] + c[8] == 'X' * 3 || 
@@ -51,7 +37,6 @@ public class TicGame {
            c[0] + c[4] + c[8] == 'X' * 3 || 
            c[2] + c[4] + c[6] == 'X' * 3 ) {
             System.out.println("Winner: X");
-            System.exit(0);
         } 
     }
     

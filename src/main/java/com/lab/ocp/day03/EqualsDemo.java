d0a967a3eff19fd50a52d9bde844975b2bebd802
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 12:08:17 +0800

    Java

--- src/main/java/com/lab/ocp/day03/EqualsDemo.java
+++ src/main/java/com/lab/ocp/day03/EqualsDemo.java
@@ -7,12 +7,18 @@ public class EqualsDemo {
         System.out.println(c1 == c2);
         String s1 = new String("Java");
         String s2 = new String("Java");
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.equalsIgnoreCase(s2)); // 不分大小寫的比較
+        System.out.println(s1 == s2); // false
+        System.out.println(s1.equals(s2)); // true
+        System.out.println(s1.equalsIgnoreCase(s2)); // 不分大小寫的比較, true
         String s3 = "Java";
         String s4 = "Java";
-        System.out.println(s3 == s4);
-        System.out.println(s3.equals(s4));
+        System.out.println(s3 == s4); // true
+        System.out.println(s3.equals(s4)); // true
+        char[] c = {'J', 'a', 'v', 'a'};
+        String s5 = new String(c);
+        System.out.println(s5);
+        System.out.println(s1.equals(s5)); // true
+        System.out.println(s3.equals(s5)); // true
+        
     }
 }

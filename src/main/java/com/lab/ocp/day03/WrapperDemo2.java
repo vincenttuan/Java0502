bc24b5948fc78186123cf471abd4e2e3ca033fe7
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 16:18:31 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day03/WrapperDemo2.java
@@ -0,0 +1,13 @@
+package com.lab.ocp.day03;
+
+public class WrapperDemo2 {
+    public static void main(String[] args) {
+        int a = 100;
+        //Integer b = Integer.valueOf("100"); // Java 5 之前
+        Integer b = 100; // Java 5 (含)之後, auto-boxing 自動裝箱
+        System.out.println(a); // int
+        System.out.println(b); // Integer
+        System.out.println(a + b.intValue()); // Java 5 之前
+        System.out.println(a + b); // Java 5 (含)之後, auto-unboxing 自動拆箱
+    }
+}

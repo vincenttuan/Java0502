4b43835df958f8cecc2e3ac9a82342aac74fc304
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 16:20:43 +0800

    Java

--- src/main/java/com/lab/ocp/day03/WrapperDemo2.java
+++ src/main/java/com/lab/ocp/day03/WrapperDemo2.java
@@ -3,8 +3,10 @@ package com.lab.ocp.day03;
 public class WrapperDemo2 {
     public static void main(String[] args) {
         int a = 100;
+        a = 200;
         //Integer b = Integer.valueOf("100"); // Java 5 之前
         Integer b = 100; // Java 5 (含)之後, auto-boxing 自動裝箱
+        b = 200;
         System.out.println(a); // int
         System.out.println(b); // Integer
         System.out.println(a + b.intValue()); // Java 5 之前

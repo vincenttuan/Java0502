a12d266b7668c241699425cf5abc8ba676b9c355
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 14:19:36 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day03/MultiArrayDemo.java
@@ -0,0 +1,19 @@
+package com.lab.ocp.day03;
+
+public class MultiArrayDemo {
+    public static void main(String[] args) {
+        int[][] nums = new int[2][3];
+        nums[0][0] = 100;
+        nums[0][1] = 90;
+        nums[0][2] = 80;
+        nums[1][0] = 70;
+        nums[1][1] = 60;
+        nums[1][2] = 50;
+        System.out.println(nums.length);
+        System.out.println(nums);
+        for(int i=0;i<nums.length;i++) {
+            System.out.println(nums[i]);
+        }
+        
+    }
+}

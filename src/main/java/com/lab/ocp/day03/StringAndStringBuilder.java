9a3c429fbe3d13379e50d4013d7499f04d21adc3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-09 14:07:15 +0800

    Java

--- src/main/java/com/lab/ocp/day03/StringAndStringBuilder.java
+++ src/main/java/com/lab/ocp/day03/StringAndStringBuilder.java
@@ -1,5 +1,20 @@
 package com.lab.ocp.day03;
 
 public class StringAndStringBuilder {
+    public static void main(String[] args) {
+        String s = "Java";
+        add(s);
+        System.out.println("s= " + s);
+        StringBuilder sb = new StringBuilder("Java");
+        add(sb);
+        System.out.println("sb= " + sb);
+    }
     
+    public static void add(String s) {
+        s = s.concat("8");
+    }
+    
+    public static void add(StringBuilder sb) {
+        sb.append("8");
+    }
 }

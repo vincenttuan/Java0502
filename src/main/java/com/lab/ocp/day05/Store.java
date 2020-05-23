7f814932e3160cf082840f00d22b9b063257746b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 10:01:57 +0800

    Java

--- src/main/java/com/lab/ocp/day05/Store.java
+++ src/main/java/com/lab/ocp/day05/Store.java
@@ -3,6 +3,12 @@ package com.lab.ocp.day05;
 public class Store {
     public static void main(String[] args) {
         Hamburger h = new Hamburger();
-        
+        h.setName("大麥克");
+        h.setPrice(70);
+        Hamburger h2 = new Hamburger();
+        h2.setName("麥香魚");
+        h2.setPrice(50);
+        System.out.printf("name: %s price: %d\n", h.getName(), h.getPrice());
+        System.out.printf("name: %s price: %d\n", h2.getName(), h2.getPrice());
     }
 }

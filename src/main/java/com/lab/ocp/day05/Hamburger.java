524547f025f2c6c196c4fe5fa06854d89c01e8b3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 09:56:15 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day05/Hamburger.java
@@ -0,0 +1,23 @@
+package com.lab.ocp.day05;
+
+public class Hamburger {
+    private String name; // 物件屬性
+    private int price; // 物件屬性
+    
+    // 封裝屬性
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}

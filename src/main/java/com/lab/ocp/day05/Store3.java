f1df584bb5f82ff5498735530c496fc5e63edca3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 10:33:48 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day05/Store3.java
@@ -0,0 +1,18 @@
+package com.lab.ocp.day05;
+
+public class Store3 {
+    public static void main(String[] args) {
+        Hamburger[] hamburgers = {
+            new Hamburger("麥香魚", 50),
+            new Hamburger("麥香魚", 60),
+            new Hamburger("大麥克", 90),
+            new Hamburger("麥香魚", 60),
+            new Hamburger("大麥克", 90),
+            new Hamburger("吉士滿意", 100),
+            new Hamburger("麥香雞", 60),
+            new Hamburger("小漢堡", 30),
+            new Hamburger("吉士漢堡", 40),
+        };
+        // 總共的價錢是 ?
+    }
+}

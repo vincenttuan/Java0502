934c850dff2c3406804eb19ca733f4fa021ef870
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 12:01:41 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day05/FastFood.java
@@ -0,0 +1,53 @@
+package com.lab.ocp.day05;
+
+public class FastFood {
+    private String name;
+    private Hamburger hamburger;
+    private SideDish sideDish;
+    private Drink drink;
+
+    public FastFood(String name) {
+        this.name = name;
+    }
+    
+    // 業務方法
+    public int getPrice() {
+        int price = hamburger.getPrice() + sideDish.getPrice() + drink.getPrice();
+        //price = (int)(price * 0.9); // 打九折
+        return price;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public Hamburger getHamburger() {
+        return hamburger;
+    }
+
+    public void setHamburger(Hamburger hamburger) {
+        this.hamburger = hamburger;
+    }
+
+    public SideDish getSideDish() {
+        return sideDish;
+    }
+
+    public void setSideDish(SideDish sideDish) {
+        this.sideDish = sideDish;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+
+    @Override
+    public String toString() {
+        return "FastFood{" + "name=" + name + ", hamburger=" + hamburger + ", sideDish=" + sideDish + ", drink=" + drink + '}';
+    }
+    
+}

466da9f29e1c9c0dcdb81b0f239bbe1b3a0f6049
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 13:51:26 +0800

    Java

--- src/main/java/com/lab/ocp/day06/SideDish.java
+++ src/main/java/com/lab/ocp/day06/SideDish.java
@@ -1,36 +1,12 @@
 package com.lab.ocp.day06;
 
-public class SideDish {
-    private String name;
-    private int price;
+public class SideDish extends Food {
 
     public SideDish() {
     }
 
     public SideDish(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "SideDish{" + "name=" + name + ", price=" + price + '}';
+        super(name, price);
     }
     
 }

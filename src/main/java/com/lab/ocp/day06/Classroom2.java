ca515c0328cb5c56a49cdfdc436454825bb74adf
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 15:00:05 +0800

    Java

--- src/main/java/com/lab/ocp/day06/Classroom2.java
+++ src/main/java/com/lab/ocp/day06/Classroom2.java
@@ -13,8 +13,18 @@ public class Classroom2 {
         String[] rows = data.split("\r\n");
         System.out.println(rows[0]);
         String[] row = rows[0].split(",");
-        System.out.println(row[0]);
-        System.out.println(row[1]);
-        System.out.println(row[2]);
+        System.out.println(row[0]); // 學生姓名
+        System.out.println(row[1]); // 考試代號/名稱
+        System.out.println(row[2]); // 考試分數
+        
+        // 建立考試成績物件
+        Exam exam = new Exam();
+        exam.setName(row[1]);
+        exam.setScore(Integer.parseInt(row[2])); // 要透過字串轉數字
+        // 建立學生物件
+        Student student = new Student();
+        student.setName(row[0]);
+        student.setExam(exam);
+        System.out.println(student);
     }
 }

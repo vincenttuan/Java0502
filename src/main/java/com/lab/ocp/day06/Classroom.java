b00e008f46a304c5b6b312baac73f01c7ab05a44
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 14:27:35 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day06/Classroom.java
@@ -0,0 +1,8 @@
+package com.lab.ocp.day06;
+
+public class Classroom {
+    public static void main(String[] args) {
+        Teacher t = new Teacher("Vincent");
+        Student s1 = new Student("John");
+    }
+}
--- src/main/java/com/lab/ocp/day06/Student.java
+++ src/main/java/com/lab/ocp/day06/Student.java
@@ -5,7 +5,11 @@ public class Student extends Person {
 
     public Student() {
     }
-
+    
+    public Student(String name) {
+        super(name);
+    }
+    
     public Student(Exam exam) {
         this.exam = exam;
     }

e432a8a7d4d613d26e625c83864bc2eebdae1544
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 10:09:30 +0800

    Java

--- src/main/java/com/lab/ocp/day05/Hamburger.java
+++ src/main/java/com/lab/ocp/day05/Hamburger.java
@@ -28,9 +28,20 @@ public class Hamburger {
     
     // 業務方法
     public void cook() {
+        System.out.println(name + " 的做法");
+        String food = "";
+        switch(name) {
+            case "大麥克":
+                food = "牛肉";
+                break;
+            case "麥香魚":
+                food = "鱈魚";
+                break;    
+        }
         System.out.println("1.放麵包");
-        System.out.println("2.放食材");
+        System.out.println("2.放" + food);
         System.out.println("3.放醬料");
         System.out.println("4.包裝");
+        System.out.println("----------------------");
     }
 }

e077844ef7bf45fa9352d22ebaf1e741a480fc88
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 15:36:24 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day06/Classroom3.java
@@ -0,0 +1,31 @@
+package com.lab.ocp.day06;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Classroom3 {
+
+    public static void main(String[] args) throws Exception {
+        // 抓取 javaexam.csv 的資料
+        File file = new File("src\\main\\java\\com\\lab\\ocp\\day06\\javaexam.csv");
+        Scanner sc = new Scanner(file);
+        String data = sc.useDelimiter("\\A").next();
+        //System.out.println(data);
+        // 切割資料 I (每一筆)
+        String[] rows = data.split("\r\n");
+        Student[] students = new Student[rows.length];
+        for (int i=0;i<rows.length;i++) {
+            String[] row = rows[i].split(",");
+            // 建立考試成績物件
+            Exam exam = new Exam();
+            exam.setName(row[1]);
+            exam.setScore(Integer.parseInt(row[2])); // 要透過字串轉數字
+            // 建立學生物件
+            Student student = new Student();
+            student.setName(row[0]);
+            student.setExam(exam);
+            //System.out.println(student);
+            students[i] = student;
+        }
+    }
+}

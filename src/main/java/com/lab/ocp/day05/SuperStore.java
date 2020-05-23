5f34ca39c68d0160a418aa04eec93936febe479d
MB-teacher <MB-teacher@192.168.1.109>
2020-05-23 12:07:10 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day05/SuperStore.java
@@ -0,0 +1,22 @@
+package com.lab.ocp.day05;
+
+public class SuperStore {
+    public static void main(String[] args) {
+        // 單品
+        Hamburger h1 = new Hamburger("大麥克", 70);
+        Hamburger h2 = new Hamburger("麥香魚", 50);
+        SideDish s1 = new SideDish("薯條", 30);
+        SideDish s2 = new SideDish("沙拉", 40);
+        Drink d1 = new Drink("可樂", 25);
+        Drink d2 = new Drink("咖啡", 45);
+        Drink d3 = new Drink("紅茶", 30);
+        // 套餐
+        FastFood f = new FastFood("一號餐");
+        f.setHamburger(h2);
+        f.setSideDish(s2);
+        f.setDrink(d3);
+        
+        System.out.println(f);
+        System.out.println("$" + f.getPrice());
+    }
+}

aea580f6c8ac229f7be04933748954501f78cd4d
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 10:13:26 +0800

    Java

--- src/main/java/com/lab/ocp/day07/MathCalcDemo.java
+++ src/main/java/com/lab/ocp/day07/MathCalcDemo.java
@@ -3,6 +3,9 @@ package com.lab.ocp.day07;
 public class MathCalcDemo {
     public static void main(String[] args) {
         System.out.println(calc(100, 10));
+        System.out.println(calc(100.0, 10.0));
+        System.out.println(calc(100.0, 10.0f));
+        System.out.println(calc(100, 10.0f));
     }
     
     public static int calc(int x, int y) {

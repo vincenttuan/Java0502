fcb93163719bed5d0ab6f4f452668c13c237efa3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 16:01:19 +0800

    Java

--- src/main/java/com/lab/ocp/day07/stock/dao/StockDao.java
+++ src/main/java/com/lab/ocp/day07/stock/dao/StockDao.java
@@ -11,7 +11,20 @@ public class StockDao {
         try {
             File file = new File("src\\main\\java\\com\\lab\\ocp\\day07\\stock\\db\\BWIBBU_d.txt");
             String data = new Scanner(file, "UTF-8").useDelimiter("\\A").next();
-            System.out.println(data);
+            String[] rows = data.split("\n");
+            stocks = new Stock[rows.length];
+            for(int i=0;i<rows.length;i++) {
+                String[] row = rows[i].split(",");
+                Stock stock = new Stock();
+                stock.set證券代號(row[0]);
+                stock.set證券名稱(row[1]);
+                stock.set殖利率(row[2]);
+                stock.set股利年度(row[3]);
+                stock.set本益比(row[4]);
+                stock.set股價淨值比(row[5]);
+                stock.set財報年季(row[6]);
+                stocks[i] = stock;
+            }
         } catch (Exception e) {
         }
     }

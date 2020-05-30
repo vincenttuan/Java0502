0a5d69a4a12cfd7bdd66eb6d7532267e6f3d130b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 10:52:01 +0800

    Java

--- src/main/java/com/lab/ocp/day07/VarargDemo.java
+++ src/main/java/com/lab/ocp/day07/VarargDemo.java
@@ -13,12 +13,6 @@ public class VarargDemo {
         System.out.println(add(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
         System.out.println(add());
     }
-    public static int add(int x, int y) {
-        return x + y;
-    }
-    public static int add(int x, int y, int z) {
-        return x + y + z;
-    }
     public static int add(int... nums) {
         return Arrays.stream(nums).sum();
     }

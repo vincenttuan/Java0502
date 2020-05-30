e760a293a6d1a853642238a5a82a6014e4253058
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 14:57:02 +0800

    Java

--- src/main/java/com/lab/ocp/day07/stock/dao/StockDao.java
+++ src/main/java/com/lab/ocp/day07/stock/dao/StockDao.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lab.ocp.day07.stock.dao;
 
-/**
- *
- * @author MB-teacher
- */
+import com.lab.ocp.day07.stock.po.Stock;
+
 public class StockDao {
+    private Stock[] stocks;
+    private static StockDao _instance = new StockDao();
+    private StockDao() {
+        stocks = new Stock[1];
+        stocks[0].set證券名稱("台積電");
+    }
+    public static StockDao getInstance() {
+        return _instance;
+    }
     
+    public Stock[] getStocks() {
+        return stocks;
+    }
 }

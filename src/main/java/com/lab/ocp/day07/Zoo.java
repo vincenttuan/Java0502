0da024e388dc090e3ae228ec50acbc270c37ee32
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 11:30:21 +0800

    Java

--- src/main/java/com/lab/ocp/day07/Zoo.java
+++ src/main/java/com/lab/ocp/day07/Zoo.java
@@ -1,11 +1,29 @@
 package com.lab.ocp.day07;
 
+import java.util.Random;
+
 public class Zoo {
     public static void main(String[] args) {
+        Animal animal = new Animal();
         Tiger tiger = new Tiger();
-        tiger.move();
         Ostrich ostrich = new Ostrich();
-        ostrich.move();
-        ostrich.motto();
+        move(ostrich);
+    }
+    public static void move(Animal animal) {
+        animal.move();
+    }
+    public static Animal getAnimal() {
+        int n = new Random().nextInt(5);
+        switch(n) {
+            case 1:
+                return new Tiger();
+            case 2:
+                return new Fish();
+            case 3:
+                return new Bird();
+            case 4:
+                return new Ostrich();    
+        }
+        return new Animal();
     }
 }

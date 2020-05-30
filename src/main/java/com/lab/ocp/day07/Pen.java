b9e9f61085734d60e36b6a3e10914197635e77f5
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 12:09:07 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day07/Pen.java
@@ -0,0 +1,33 @@
+package com.lab.ocp.day07;
+
+public class Pen {
+    private String color;
+    private int price;
+
+    public Pen(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen{" + "color=" + color + ", price=" + price + '}';
+    }
+    
+}

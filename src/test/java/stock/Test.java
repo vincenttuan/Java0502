398ee255373209b3b339eb448120c8da1cd28cf7
MB-teacher <MB-teacher@192.168.1.109>
2020-05-30 16:03:27 +0800

    Java

--- src/test/java/stock/Test.java
+++ src/test/java/stock/Test.java
@@ -1,11 +1,14 @@
 package stock;
 
 import com.lab.ocp.day07.stock.dao.StockDao;
+import com.lab.ocp.day07.stock.po.Stock;
+import java.util.Arrays;
 
 public class Test {
     @org.junit.Test
     public void test_StockDao() {
         StockDao dao = StockDao.getInstance();
-        
+        Stock[] stocks = dao.getStocks();
+        Arrays.stream(stocks).forEach(s -> System.out.println(s.get證券名稱()));
     }
 }

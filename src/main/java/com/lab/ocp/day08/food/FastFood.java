16218b9fed6b97da548a81248d1a698bedc39340
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 10:40:26 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day08/food/FastFood.java
@@ -0,0 +1,7 @@
+package com.lab.ocp.day08.food;
+
+public interface FastFood extends Food {
+    // 數量 getter / setter
+    Integer getCount();
+    void setCount(Integer count);
+}
---
+++ src/main/java/com/lab/ocp/day08/food/Food.java
@@ -0,0 +1,10 @@
+package com.lab.ocp.day08.food;
+
+public interface Food {
+    // 品名 getter/setter
+    String getName();
+    void setName(String name);
+    // 價格 getter/setter
+    Integer getPrice();
+    void setPrice(Integer price);
+}

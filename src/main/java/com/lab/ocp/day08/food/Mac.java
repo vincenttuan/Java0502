5d1ab8801d0de22e055a5a8a7d05b06bcb3d3b49
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 10:47:04 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day08/food/Mac.java
@@ -0,0 +1,37 @@
+package com.lab.ocp.day08.food;
+
+public class Mac implements FastFood {
+    private String name;
+    private Integer price;
+    private Integer count;
+    @Override
+    public Integer getCount() {
+        return count;
+    }
+
+    @Override
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+}

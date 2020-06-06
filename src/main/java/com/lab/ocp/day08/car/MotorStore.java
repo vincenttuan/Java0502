31eb4432a45fee39fea243a607b2afbb93c59250
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 12:06:45 +0800

    Java

--- src/main/java/com/lab/ocp/day08/car/MotorStore.java
+++ src/main/java/com/lab/ocp/day08/car/MotorStore.java
@@ -1,5 +1,7 @@
 package com.lab.ocp.day08.car;
 
+import java.util.Arrays;
+
 public class MotorStore {
     public static void main(String[] args) {
         Motor b1 = new BMW();
@@ -9,8 +11,16 @@ public class MotorStore {
         Motor b5 = new Motorcycle();
         Motor b6 = new Motorcycle();
         Motor b7 = new Motorcycle();
-        Motor[] motors = {b1, b2, b3, b4, b5, b6};
+        Motor[] motors = {b1, b2, b3, b4, b5, b6, b7};
         // 求 汽車/摩托車總價各是多少 ?
-        
+        int sum_car = Arrays.stream(motors)
+                .filter(m -> m instanceof Car)
+                .mapToInt(m -> m.getPrice())
+                .sum();
+        int sum_motocycle = Arrays.stream(motors)
+                .filter(m -> m.getKind().equals("摩托車"))
+                .mapToInt(m -> m.getPrice())
+                .sum();
+        System.out.printf("汽車總價: $%,d 摩托車總價: $%,d\n", sum_car, sum_motocycle);
     }
 }

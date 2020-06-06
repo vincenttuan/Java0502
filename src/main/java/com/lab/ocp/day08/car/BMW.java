d665bdcc80f7650f2fc7a9cb84c3f5248e2e1c1e
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 12:00:07 +0800

    Java

--- src/main/java/com/lab/ocp/day08/car/BMW.java
+++ src/main/java/com/lab/ocp/day08/car/BMW.java
@@ -1,7 +1,7 @@
 package com.lab.ocp.day08.car;
 
 public class BMW extends Car {
-
+    
     @Override
     public Integer getPrice() {
         return 200_0000;
---
+++ src/main/java/com/lab/ocp/day08/car/MotorStore.java
@@ -0,0 +1,16 @@
+package com.lab.ocp.day08.car;
+
+public class MotorStore {
+    public static void main(String[] args) {
+        Motor b1 = new BMW();
+        Motor b2 = new BMW();
+        Motor b3 = new Banz();
+        Motor b4 = new Banz();
+        Motor b5 = new Motorcycle();
+        Motor b6 = new Motorcycle();
+        Motor b7 = new Motorcycle();
+        Motor[] motors = {b1, b2, b3, b4, b5, b6};
+        // 求 汽車/摩托車總價各是多少 ?
+        
+    }
+}

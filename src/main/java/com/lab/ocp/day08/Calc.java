4a69596724a33dd60473779fe270fae2d425f751
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 16:20:06 +0800

    Java

--- src/main/java/com/lab/ocp/day08/Calc.java
+++ src/main/java/com/lab/ocp/day08/Calc.java
@@ -1,5 +1,6 @@
 package com.lab.ocp.day08;
 
+@FunctionalInterface
 public interface Calc {
     double cal(double x, double y);
 }
--- src/main/java/com/lab/ocp/day08/CalcDemo.java
+++ src/main/java/com/lab/ocp/day08/CalcDemo.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lab.ocp.day08;
 
-/**
- *
- * @author MB-teacher
- */
 public class CalcDemo {
-    
+    public static void main(String[] args) {
+        Calc add = new Calc(){
+            @Override
+            public double cal(double x, double y) {
+                return x + y;
+            }
+        };
+        Calc sub = new Calc(){
+            @Override
+            public double cal(double x, double y) {
+                return x - y;
+            }
+        };
+        
+        Calc mul = (double x, double y) -> {return x * y;};
+        
+        System.out.println(add.cal(10, 20));
+        System.out.println(sub.cal(10, 20));
+        System.out.println(mul.cal(10, 20));
+    }
 }

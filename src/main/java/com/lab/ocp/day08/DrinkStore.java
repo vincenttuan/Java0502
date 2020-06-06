581e60e1c90f2e23cd016e3ede68763f0862b1fc
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 10:04:50 +0800

    Java

--- src/main/java/com/lab/ocp/day08/DrinkStore.java
+++ src/main/java/com/lab/ocp/day08/DrinkStore.java
@@ -2,7 +2,20 @@ package com.lab.ocp.day08;
 
 public class DrinkStore {
     public static void main(String[] args) {
-        Drink d1 = new Coffee();
+        Drink c1 = new Coffee("拿鐵", 50);
+        Drink c2 = new Coffee("美式", 40);
+        Drink c3 = new Coffee("焦糖", 60);
+        
+        Drink t1 = new Tea("紅茶", 20);
+        Drink t2 = new Tea("綠茶", 25);
+        
+        Drink m1 = new Milk("巧克力", 45);
+        Drink m2 = new Milk("鮮奶", 45);
+        
+        Drink j1 = new Juice("葡萄汁", 40);
+        Drink j2 = new Juice("柳橙汁", 35);
+        
+        Drink[] drinks = {c1, c2, c3, t1, t2, m1, m2, j1, j2};
         
     }
 }

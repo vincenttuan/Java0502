4d5f78000f87b2ea51ad5d8cdb2082f6094e2db5
MB-teacher <MB-teacher@192.168.1.109>
2020-06-06 16:29:25 +0800

    Java

--- src/main/java/com/lab/ocp/day08/CalcDemo.java
+++ src/main/java/com/lab/ocp/day08/CalcDemo.java
@@ -17,8 +17,17 @@ public class CalcDemo {
         
         Calc mul = (double x, double y) -> {return x * y;};
         
+        Calc div = (x, y) -> x / y  ;
+        
+        Calc max = (x, y) -> Math.max(x, y);
+        
+        Calc max2 = Math::max; // :: 稱為方法參考
+        
         System.out.println(add.cal(10, 20));
         System.out.println(sub.cal(10, 20));
         System.out.println(mul.cal(10, 20));
+        System.out.println(div.cal(10, 20));
+        System.out.println(max.cal(10, 20));
+        System.out.println(max2.cal(10, 20));
     }
 }

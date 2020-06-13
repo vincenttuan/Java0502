ccad071e911deb058c768aecebb14923b46d4a10
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 11:45:49 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day09/Student.java
@@ -0,0 +1,63 @@
+package com.lab.ocp.day09;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class Student {
+    private String name;
+    private Set<Exam> exams;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Exam> getExams() {
+        return exams;
+    }
+
+    public void setExams(Set<Exam> exams) {
+        this.exams = exams;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + Objects.hashCode(this.exams);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.exams, other.exams)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", exams=" + exams + '}';
+    }
+    
+}

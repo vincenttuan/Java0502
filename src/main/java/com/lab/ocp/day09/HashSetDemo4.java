c69b19e58e58b3d19941431e50fbd62ab99ea1a7
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 10:28:44 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day09/HashSetDemo4.java
@@ -0,0 +1,25 @@
+package com.lab.ocp.day09;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetDemo4 {
+    public static void main(String[] args) {
+        Set hs = new HashSet();
+        hs.add("國文");hs.add(100);
+        hs.add("數學");hs.add(90);
+        hs.add("英文");hs.add(100);
+        hs.add(null);hs.add(3.14);
+        System.out.println(hs);
+        Iterator it = hs.iterator();
+        while (it.hasNext()) {
+            Object next = it.next();
+            if(!(next instanceof String)) {
+                it.remove();
+            }
+        }
+        System.out.println(hs);
+        
+    }
+}

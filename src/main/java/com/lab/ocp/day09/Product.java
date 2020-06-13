dd399d76860b6ddd69b070a8ee9db306d46a432d
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 13:50:33 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day09/Product.java
@@ -0,0 +1,81 @@
+package com.lab.ocp.day09;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private Integer amount;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Integer amount, Double price) {
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.amount);
+        hash = 89 * hash + Objects.hashCode(this.price);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.amount, other.amount)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name=" + name + ", amount=" + amount + ", price=" + price + '}';
+    }
+    
+}

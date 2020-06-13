5f146aa3376b6328ea6b5e5fda91e39ca122da97
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 14:19:07 +0800

    Java

--- src/main/java/com/lab/ocp/day09/ProductDemo.java
+++ src/main/java/com/lab/ocp/day09/ProductDemo.java
@@ -1,16 +1,25 @@
 package com.lab.ocp.day09;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class ProductDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 將 products.json 資料匯入
-        TreeSet<Product> products = new TreeSet<>();
-        products.add(new Product("Pen", 10, 6.5));
-        products.add(new Product("Book", 3, 12.5));
+        File file = new File("src\\main\\java\\com\\lab\\ocp\\day09\\products.json");
+        String json = new Scanner(file).useDelimiter("\\A").next();
+        Type type = new TypeToken<TreeSet<Product>>() {}.getType();
+        TreeSet<Product> products = new Gson().fromJson(json, type);
         
         // 顯示 products 的內容
         System.out.println(products);
+        
+        // 請問總共要花多少錢 ?
+        
     }
 }

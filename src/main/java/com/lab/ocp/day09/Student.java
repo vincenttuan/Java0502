aee92ea6ea01e42af9d31eed3cb2d38482fe83b9
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 12:19:26 +0800

    Java

--- src/main/java/com/lab/ocp/day09/Student.java
+++ src/main/java/com/lab/ocp/day09/Student.java
@@ -1,14 +1,8 @@
 package com.lab.ocp.day09;
 
-import java.util.Objects;
-import java.util.Set;
-
 public class Student {
     private String name;
-    private Set<Exam> exams;
-
-    public Student() {
-    }
+    private Exam[] exams;
 
     public String getName() {
         return name;
@@ -18,46 +12,18 @@ public class Student {
         this.name = name;
     }
 
-    public Set<Exam> getExams() {
+    public Exam[] getExams() {
         return exams;
     }
 
-    public void setExams(Set<Exam> exams) {
+    public void setExams(Exam[] exams) {
         this.exams = exams;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 41 * hash + Objects.hashCode(this.name);
-        hash = 41 * hash + Objects.hashCode(this.exams);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Student other = (Student) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.exams, other.exams)) {
-            return false;
-        }
-        return true;
     }
 
     @Override
     public String toString() {
         return "Student{" + "name=" + name + ", exams=" + exams + '}';
     }
+
     
 }
--- src/main/java/com/lab/ocp/day09/StudentDemo.java
+++ src/main/java/com/lab/ocp/day09/StudentDemo.java
@@ -2,6 +2,8 @@ package com.lab.ocp.day09;
 
 import com.google.gson.Gson;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -10,10 +12,11 @@ public class StudentDemo {
         File file = new File("src\\main\\java\\com\\lab\\ocp\\day09\\exams.json");
         String json = new Scanner(file).useDelimiter("\\A").next();
         Gson gson = new Gson();
-        Set<Student> students = gson.fromJson(json, Set.class);
+        Student[] students = gson.fromJson(json, Student[].class);
         System.out.println(students);
         
         // 請求出每人的總分各是多少 ?
+        
         
         // 請求出總分最高的人名 ?
         

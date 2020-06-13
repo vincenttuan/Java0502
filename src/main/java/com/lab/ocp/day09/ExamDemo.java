2928cd5a9e890f39d6e18d12500608d8bb0ffa57
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 10:50:25 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day09/ExamDemo.java
@@ -0,0 +1,16 @@
+package com.lab.ocp.day09;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExamDemo {
+    public static void main(String[] args) {
+        Exam e1 = new Exam("國文", 100);
+        Exam e2 = new Exam("數學", 80);
+        Exam e3 = new Exam("英文", 100);
+        Set<Exam> exams = new HashSet<>();
+        exams.add(e1);exams.add(e2);exams.add(e3);
+        System.out.println(exams);
+        
+    }
+}

b26dc9a97f3e4b419ec0cf9ca6910b4193c50587
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 10:33:34 +0800

    Java

--- src/main/java/com/lab/ocp/day09/HashSetDemo4.java
+++ src/main/java/com/lab/ocp/day09/HashSetDemo4.java
@@ -3,6 +3,7 @@ package com.lab.ocp.day09;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import static java.util.stream.Collectors.toSet;
 
 public class HashSetDemo4 {
     public static void main(String[] args) {
@@ -20,6 +21,18 @@ public class HashSetDemo4 {
             }
         }
         System.out.println(hs);
+        //-------------------------------------
+        hs = new HashSet();
+        hs.add("國文");hs.add(100);
+        hs.add("數學");hs.add(90);
+        hs.add("英文");hs.add(100);
+        hs.add(null);hs.add(3.14);
+        System.out.println(hs);
+        
+        Set hs2 = (Set)hs.stream()
+                .filter(obj -> obj instanceof String)
+                .collect(toSet());
+        System.out.println(hs2);
         
     }
 }

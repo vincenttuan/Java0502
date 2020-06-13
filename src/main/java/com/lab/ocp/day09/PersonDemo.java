6783c1ad65e5c7a65735f20d4816100dccd427b4
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 13:40:16 +0800

    Java

--- src/main/java/com/lab/ocp/day09/PersonDemo.java
+++ src/main/java/com/lab/ocp/day09/PersonDemo.java
@@ -8,13 +8,19 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class PersonDemo {
+
     public static void main(String[] args) throws Exception {
         File file = new File("src\\main\\java\\com\\lab\\ocp\\day09\\exams.json");
         String json = new Scanner(file).useDelimiter("\\A").next();
-        
-        Type type = new TypeToken<Set<Person>>() {}.getType();
+
+        Type type = new TypeToken<Set<Person>>() {
+        }.getType();
         Set<Person> persons = new Gson().fromJson(json, type);
-        persons.stream().forEach(p -> System.out.println(p.getName()));
-        
+        persons.stream().forEach(p -> {
+            int sum = p.getExams().stream().mapToInt(e -> e.getScore()).sum();
+            System.out.printf("%s %d\n", p.getName(), sum);
+            
+        });
+
     }
 }

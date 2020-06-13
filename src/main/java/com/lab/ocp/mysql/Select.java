7276a7b7683d9c076cac04f1e497b5b727262afa
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 15:57:16 +0800

    Java

---
+++ src/main/java/com/lab/ocp/mysql/Select.java
@@ -0,0 +1,16 @@
+package com.lab.ocp.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Select {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/ocp?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
+        String username = "root";
+        String password = "1234";
+        Connection conn = DriverManager.getConnection(url, username, password);
+        System.out.println(!conn.isClosed());
+        
+    }
+}

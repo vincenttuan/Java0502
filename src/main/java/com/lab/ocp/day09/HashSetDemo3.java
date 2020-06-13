1412e9ec5cb45ce2580c0c5650bdc322739729d6
MB-teacher <MB-teacher@192.168.1.109>
2020-06-13 10:14:02 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day09/HashSetDemo3.java
@@ -0,0 +1,25 @@
+package com.lab.ocp.day09;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo3 {
+    public static void main(String[] args) {
+        Set hs = new HashSet();
+        hs.add("國文");hs.add(100);
+        hs.add("數學");hs.add(90);
+        hs.add("英文");hs.add(100);
+        hs.add(null);hs.add(3.14);
+        System.out.println(hs);
+        // 求出元素中整數資料相加的結果
+        // Java 7
+        int sum = 0;
+        for(Object obj : hs) {
+            if(obj instanceof Integer) {
+                sum += (Integer)obj;
+            }
+        }
+        System.out.println(sum);
+        
+    }
+}

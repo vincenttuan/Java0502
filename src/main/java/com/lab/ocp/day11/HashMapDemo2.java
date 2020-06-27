e29427cf80a543b495f4ac529ac86bd667f6e07c
MB-teacher <MB-teacher@192.168.1.109>
2020-06-27 14:10:04 +0800

    Java

--- src/main/java/com/lab/ocp/day11/HashMapDemo2.java
+++ src/main/java/com/lab/ocp/day11/HashMapDemo2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class HashMapDemo2 {
@@ -12,7 +13,9 @@ public class HashMapDemo2 {
         List<Map> list = new ArrayList<>();
         IntStream.rangeClosed(1, 100).forEach(i -> {
             Map<String, Object> map = new LinkedHashMap<>();
-            
+            map.put("height", new Random().nextInt(50) + 150);
+            map.put("weight", new Random().nextInt(50) + 50);
+            map.put("name", "John");
             list.add(map);
         });
         System.out.println(list);

cf787cdb0326d855793eebdebbc1b4d27d273062
MB-teacher <MB-teacher@192.168.1.109>
2020-06-27 10:02:22 +0800

    Java

--- src/main/java/com/lab/ocp/day10/LinkedListDemo.java
+++ src/main/java/com/lab/ocp/day10/LinkedListDemo.java
@@ -2,6 +2,7 @@ package com.lab.ocp.day10;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class LinkedListDemo {
     public static void main(String[] args) {
@@ -16,5 +17,18 @@ public class LinkedListDemo {
         //int sum = list.stream().mapToInt(Integer::valueOf).sum();
         int sum = list.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
+        // 利用 ListIterator 走訪元素
+        ListIterator<Integer> iter = list.listIterator();
+        while(iter.hasNext()) {
+            int idx = iter.nextIndex();
+            Integer value = iter.next();
+            System.out.printf("%d : %d\n", idx, value);
+        }
+        while(iter.hasPrevious()) {
+            int idx = iter.previousIndex();
+            Integer value = iter.previous();
+            System.out.printf("%d : %d\n", idx, value);
+        }
+        
     }
 }

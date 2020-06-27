af3e2651a49e83fa84b67434e8ca2650780dfbce
MB-teacher <MB-teacher@192.168.1.109>
2020-06-27 11:50:01 +0800

    Java

--- src/main/java/com/lab/ocp/day10/junior/JuniorDemo.java
+++ src/main/java/com/lab/ocp/day10/junior/JuniorDemo.java
@@ -3,10 +3,21 @@ package com.lab.ocp.day10.junior;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JuniorDemo {
     public static void main(String[] args) throws Exception {
         Path path = Paths.get("src\\main\\java\\com\\lab\\ocp\\day10\\junior\\junior.txt");
-        Files.lines(path).forEach(System.out::println);
+        List<Junior> list = new ArrayList<>();
+        Files.lines(path).forEach(record -> {
+            String[] rows = record.split(",");
+            Junior junior = new Junior();
+            junior.setSchool(rows[0].trim());
+            junior.setAmount(Integer.parseInt(rows[1].trim()));
+            junior.setScore(Double.parseDouble(rows[2].trim()));
+            list.add(junior);
+        });
+        System.out.println(list);
     }
 }

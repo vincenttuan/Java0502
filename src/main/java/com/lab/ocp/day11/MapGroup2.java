8cd0625b8a1b21798deaf29e1b1bbf2ea65042a5
MB-teacher <MB-teacher@192.168.1.109>
2020-06-27 16:06:22 +0800

    Java

--- src/main/java/com/lab/ocp/day11/MapGroup2.java
+++ src/main/java/com/lab/ocp/day11/MapGroup2.java
@@ -1,9 +1,12 @@
 package com.lab.ocp.day11;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
+import javax.swing.text.html.parser.Entity;
 
 public class MapGroup2 {
     public static void main(String[] args) {
@@ -12,7 +15,14 @@ public class MapGroup2 {
                            "banana", "coconut", "orange"};
         // 3:[apple], 2:[watermelon, orange], 1:[banana,coconut]
         // 分組
-        
+        System.out.println(
+            Stream.of(fruits)
+                  .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                  .entrySet() // [banana=1, orange=2, apple=3, watermelon=2, coconut=1]
+                  .stream()
+                  .collect(Collectors.groupingBy(Entry::getValue, 
+                                                 Collectors.mapping(Entry::getKey, toList())))
+        );
         
     }
 }

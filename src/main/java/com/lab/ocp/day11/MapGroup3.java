4c7a74d5870e4de4a319b0f6554f6d69b0ad868d
MB-teacher <MB-teacher@192.168.1.109>
2020-06-27 16:10:50 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day11/MapGroup3.java
@@ -0,0 +1,22 @@
+package com.lab.ocp.day11;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+import javax.swing.text.html.parser.Entity;
+
+public class MapGroup3 {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "apple", "watermelon", 
+                           "apple", "orange", "watermelon", 
+                           "banana", "coconut", "orange"};
+        // apple:5, banana:6, orange:6, coconut:7, watermelon:10
+        // 5:[apple], 6:[banana, orange], 7:[coconut], 10:[watermelon]
+        // 分組
+        
+        
+    }
+}

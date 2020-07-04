d7114f777facbc123807db9398989208f1dcd211
MB-teacher <MB-teacher@192.168.1.109>
2020-07-04 15:38:49 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day11/Student.java
@@ -0,0 +1,60 @@
+package com.lab.ocp.day11;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + this.age;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", age=" + age + '}';
+    }
+    
+}
---
+++ src/main/java/com/lab/ocp/day11/TreeMapDemo.java
@@ -0,0 +1,7 @@
+package com.lab.ocp.day11;
+
+
+
+public class TreeMapDemo {
+    
+}

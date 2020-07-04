83d91208d300c0a6dd85fc18a2b0f045adfe8a95
MB-teacher <MB-teacher@192.168.1.109>
2020-07-04 12:28:49 +0800

    Java

--- src/main/java/com/lab/ocp/day12/OpenWeatherDemo.java
+++ src/main/java/com/lab/ocp/day12/OpenWeatherDemo.java
@@ -3,6 +3,8 @@ package com.lab.ocp.day12;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -42,7 +44,15 @@ public class OpenWeatherDemo {
             String icon = wObj.get("icon").getAsString();
             
             System.out.printf("溫度: %.2f °C 體感: %.1f °C 濕度: %.1f %% (%s)\n", temp, feels_like, humidity, description);
-            System.out.println(String.format(path_icon, icon));
+            path_icon = String.format(path_icon, icon);
+            System.out.println(path_icon);
+            // 將 icon 圖片存檔
+            InputStream is_icon = new URL(path_icon).openStream();
+            FileOutputStream fos = new FileOutputStream("src/main/java/com/lab/ocp/day12/" + icon + ".png");
+            byte[] bytes = new byte[is_icon.available()];
+            is_icon.read(bytes);
+            fos.write(bytes);
+            
         } catch (MalformedURLException ex) {
             System.out.println("網路路徑格式錯誤, 錯誤原因: " + ex);
         } catch (IOException ex) {

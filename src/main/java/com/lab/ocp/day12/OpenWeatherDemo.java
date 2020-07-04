31c6759d5c746914a554c416460121902ed2a135
MB-teacher <MB-teacher@192.168.1.109>
2020-07-04 11:42:01 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day12/OpenWeatherDemo.java
@@ -0,0 +1,13 @@
+package com.lab.ocp.day12;
+
+public class OpenWeatherDemo {
+    private static String key = "a82e36203e1d003a2e10a186c5e939a3";
+    private static String path = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s";
+    public static void main(String[] args) {
+        printWeather("taoyuan");
+    }
+    
+    public static void printWeather(String city) {
+        
+    }
+}

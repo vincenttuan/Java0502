8f4703556f034a8d438d47275db92124d5751477
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 16:27:10 +0800

    Java

--- src/main/java/com/lab/ocp/day14/thread/PapayaMilk.java
+++ src/main/java/com/lab/ocp/day14/thread/PapayaMilk.java
@@ -19,18 +19,35 @@ class BuyMilk implements Callable<String> {
     }
 }
 
+class BuyIce implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+        System.out.println("在 run() 中印出冰塊");
+    }
+}
+
 public class PapayaMilk {
     public static void main(String[] args) throws Exception {
         BuyPapaya papaya = new BuyPapaya();
         BuyMilk milk = new BuyMilk();
+        BuyIce ice = new BuyIce();
         FutureTask<String> task1 = new FutureTask<>(papaya);
         FutureTask<String> task2 = new FutureTask<>(milk);
+        FutureTask<String> task3 = new FutureTask<>(ice, "冰塊");
         new Thread(task1).start();
         new Thread(task2).start();
+        new Thread(task3).start();
         String data1 = task1.get();
         System.out.printf("買到 %s\n", data1);
         String data2 = task2.get();
         System.out.printf("買到 %s\n", data2);
-        System.out.printf("製作 %s\n", (data1 + data2));
+        String data3 = task3.get();
+        System.out.printf("買到 %s\n", data3);
+        
+        System.out.printf("製作 %s\n", (data1 + data2 + data3));
     }
 }

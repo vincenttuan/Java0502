13a851a6daa1aab3f6c12e3e1b2fa3130e7400c3
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 15:46:04 +0800

    Java

--- src/main/java/com/lab/ocp/day14/thread/RaceDemo.java
+++ src/main/java/com/lab/ocp/day14/thread/RaceDemo.java
@@ -3,7 +3,7 @@ package com.lab.ocp.day14.thread;
 class Race implements Runnable {
     @Override
     public void run() {
-        for(int i=1;i<=Integer.MAX_VALUE;i++); // 跑步過程(所花的時間)
+        for(int i=1;i<=100_0000;i++); // 跑步過程(所花的時間)
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 到達終點\n", tName);
     }
@@ -14,6 +14,8 @@ public class RaceDemo {
         Race race = new Race();
         Thread t1 = new Thread(race, "兔子");
         Thread t2 = new Thread(race, "烏龜");
+        t1.setPriority(Thread.MIN_PRIORITY); // 1
+        t2.setPriority(10);
         t1.start();
         t2.start();
     }

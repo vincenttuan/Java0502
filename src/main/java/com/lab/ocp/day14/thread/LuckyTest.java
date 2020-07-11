0893cd66330d03eb6f8bb0d87fdb4a0abf63459e
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 12:16:31 +0800

    Java

--- src/main/java/com/lab/ocp/day14/thread/LuckyTest.java
+++ src/main/java/com/lab/ocp/day14/thread/LuckyTest.java
@@ -11,16 +11,26 @@ class LuckyNumber extends Thread {
     public void run() {
         String tname = Thread.currentThread().getName();
         // code 1
-        while (true) {            
+        int count = 0;
+        while (true) {
+            ++count;
             int n = new Random().nextInt(1000);
             // code 2
+            if (n == 777) {
+                break;
+            }
         }
         // 輸出結果範例:小華 總共取了 261 次才得到 777
         // code 3
+        System.out.printf("%s 總共取了 %d 次才得到 777\n", tname, count);
     }
     
 }
 
 public class LuckyTest {
     // code 4
+    public static void main(String[] args) {
+        new LuckyNumber("小明").start();
+        new LuckyNumber("小華").start();
+    }
 }

8d49cc2de206d15f372682010498c48fd5d2f714
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 11:59:38 +0800

    Java

--- src/main/java/com/lab/ocp/day14/thread/Runner.java
+++ src/main/java/com/lab/ocp/day14/thread/Runner.java
@@ -12,6 +12,13 @@ public class Runner extends Thread {
         String tname = Thread.currentThread().getName();
         for(int i=1;i<=1000;i++) {
             System.out.printf("%s 跑了 %d 步\n", tname, i);
+            if(tname.equals("兔子") && i == 500) {
+                try {
+                    System.out.println(tname + "睡覺中...");
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                }
+            }
         }
     }
 }

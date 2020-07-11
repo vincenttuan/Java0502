299728db061f128615080629442b4643aa08b533
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 11:54:34 +0800

    Java

--- src/main/java/com/lab/ocp/day14/thread/RunnerTest2.java
+++ src/main/java/com/lab/ocp/day14/thread/RunnerTest2.java
@@ -4,6 +4,8 @@ public class RunnerTest2 {
     public static void main(String[] args) {
         Runner r1 = new Runner();
         Runner r2 = new Runner();
+        r1.setName("烏龜");
+        r2.setName("兔子");
         r1.start(); // 下達 start() 方法後會進入"排程"並執行 run() 方法
         r2.start();
     }

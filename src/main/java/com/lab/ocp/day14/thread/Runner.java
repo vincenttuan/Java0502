2bd1a0f8e95456a50dabda70b9b546d78a8f847e
MB-teacher <MB-teacher@192.168.1.109>
2020-07-11 11:47:21 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day14/thread/Runner.java
@@ -0,0 +1,12 @@
+package com.lab.ocp.day14.thread;
+
+public class Runner {
+    
+    public void job() {
+        String tname = Thread.currentThread().getName();
+        for(int i=1;i<=1000;i++) {
+            System.out.printf("%s 跑了 %d 步\n", tname, i);
+        }
+    }
+    
+}

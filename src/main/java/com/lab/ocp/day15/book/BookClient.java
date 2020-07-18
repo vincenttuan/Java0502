c21fe7b5c22c60b4d2c7273799fd82efbc9558c1
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 11:37:30 +0800

    Java

--- src/main/java/com/lab/ocp/day15/book/BookClient.java
+++ src/main/java/com/lab/ocp/day15/book/BookClient.java
@@ -3,7 +3,10 @@ package com.lab.ocp.day15.book;
 public class BookClient {
     public static void main(String[] args) {
         BookStore bookStore = new BookStore();
+        BookStore bookStore2 = new BookStore();
         Thread t1 = new Thread(new Borrow(bookStore), "小明");
+        Thread t2 = new Thread(new Borrow(bookStore2), "小華");
         t1.start();
+        t2.start();
     }
 }

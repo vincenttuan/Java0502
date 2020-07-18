28a12b12d403188b54a1e4180e59ecae7b54c0ac
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 10:50:16 +0800

    Java

--- src/main/java/com/lab/ocp/day15/atm/ATM.java
+++ src/main/java/com/lab/ocp/day15/atm/ATM.java
@@ -4,6 +4,10 @@ public class ATM {
     public static void main(String[] args) {
         Account account = new Account(10000);
         Thread t1 = new Thread(new Withdraw(account, 6000), "小明");
+        Thread t2 = new Thread(new Withdraw(account, 3000), "小華");
+        Thread t3 = new Thread(new Withdraw(account, 2000), "小英");
         t1.start();
+        t2.start();
+        t3.start();
     }
 }

a22c15e73fe7311cbbef9ef605a94046654cf318
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 12:22:07 +0800

    Java

--- src/main/java/com/lab/ocp/day15/wait_notify/Cookie.java
+++ src/main/java/com/lab/ocp/day15/wait_notify/Cookie.java
@@ -3,20 +3,20 @@ package com.lab.ocp.day15.wait_notify;
 public class Cookie {
     private boolean empty = true; // 空盤子
     public synchronized void put(int num) throws InterruptedException {
-        if(!empty) {
+        while(!empty) {
             wait();
         }
         System.out.printf("主人放第 %d 塊餅乾\n", num);
         empty = false;
-        notify();
+        notifyAll();
     }
     
     public synchronized void eat(int num) throws InterruptedException {
-        if(empty) {
+        while(empty) {
             wait();
         }
         System.out.printf("小狗吃第 %d 塊餅乾\n", num);
         empty = true;
-        notify();
+        notifyAll();
     }
 }

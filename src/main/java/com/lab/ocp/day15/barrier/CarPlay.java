cc5d7e73339f3a79c13ab0cac914a141fb4e0fa0
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 14:56:04 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day15/barrier/CarPlay.java
@@ -0,0 +1,14 @@
+package com.lab.ocp.day15.barrier;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CarPlay {
+    public static void main(String[] args) {
+        CyclicBarrier cb = new CyclicBarrier(3);
+        for(int i=1;i<=3;i++) {
+            Car car = new Car(cb);
+            car.start();
+        }
+        
+    }
+}

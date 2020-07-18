a5f58eef3641d29cbdeff7fe5bc7b51ed69ca59f
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 10:45:23 +0800

    Java

--- src/main/java/com/lab/ocp/day15/atm/Withdraw.java
+++ src/main/java/com/lab/ocp/day15/atm/Withdraw.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lab.ocp.day15.atm;
 
-/**
- *
- * @author MB-teacher
- */
-public class Withdraw {
+public class Withdraw implements Runnable {
+    private Account account;
+    private int money;
+
+    public Withdraw(Account account, int money) {
+        this.account = account;
+        this.money = money;
+    }
+    
+    @Override
+    public void run() {
+        account.widthdraw(money);
+    }
     
 }

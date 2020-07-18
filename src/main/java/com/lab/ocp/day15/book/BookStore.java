807e50d80d670050453916cdc3438ecaa2c192f2
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 11:41:59 +0800

    Java

--- src/main/java/com/lab/ocp/day15/book/BookStore.java
+++ src/main/java/com/lab/ocp/day15/book/BookStore.java
@@ -3,9 +3,14 @@ package com.lab.ocp.day15.book;
 public class BookStore {
     public static int amount = 100;
     public void borrow() { // 借書
+        
         String tname = Thread.currentThread().getName();
         for(int i=0;i<Integer.MAX_VALUE;i++);
-        System.out.printf("%s 借了第 %d 本書\n", tname, amount);
-        amount--;
+        synchronized(BookStore.class) {
+            if(amount <= 0) return;
+            System.out.printf("%s 借了第 %d 本書\n", tname, amount);
+            amount--;
+        }
+        
     }
 }

5f35080348a876950df73c81235def677fae34bd
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 14:52:48 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day15/barrier/Car.java
@@ -0,0 +1,29 @@
+package com.lab.ocp.day15.barrier;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car extends Thread {
+    private CyclicBarrier cb;
+
+    public Car(CyclicBarrier cb) {
+        this.cb = cb;
+    }
+    
+    @Override
+    public void run() {
+        String tname = Thread.currentThread().getName();
+        System.out.printf("%s 從台北出發...\n", tname);
+        try {
+            Thread.sleep(new Random().nextInt(5000));
+            System.out.printf("%s 到台中了!\n", tname);
+            cb.await(); // 等待其他執行緒
+        } catch (Exception e) {
+            System.out.printf("%s 發生車禍了!\n", tname);
+            return;
+        }
+        System.out.printf("%s 繼續往高雄出發...\n", tname);
+        
+    }
+    
+}

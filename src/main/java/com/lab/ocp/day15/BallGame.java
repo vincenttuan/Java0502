36664c52908e15621fff1e8080132b9a9f9a2a0f
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 10:21:41 +0800

    Java

--- src/main/java/com/lab/ocp/day15/BallGame.java
+++ src/main/java/com/lab/ocp/day15/BallGame.java
@@ -1,13 +1,19 @@
 package com.lab.ocp.day15;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BallGame implements Runnable {
     private int amount = 1000;
-
+    private Map<String, Integer> map = new LinkedHashMap<>();
     @Override
     public void run() {
+        String tname = Thread.currentThread().getName();
+        map.put(tname, 0);
         while (amount > 0) {            
             get();
         }
+        System.out.println(map);
     }
     
     public synchronized void get() {
@@ -15,5 +21,6 @@ public class BallGame implements Runnable {
         String tname = Thread.currentThread().getName();
         System.out.printf("%s 取到第 %d 顆球\n", tname, amount);
         amount--;
+        map.put(tname, map.get(tname) + 1);
     }
 }

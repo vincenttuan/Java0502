7198e044823c8f94390ddd29fce6020c51f6b839
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 09:46:49 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day15/BallGame.java
@@ -0,0 +1,16 @@
+package com.lab.ocp.day15;
+
+public class BallGame extends Thread {
+    private int amount = 10;
+
+    @Override
+    public void run() {
+        get();
+    }
+    
+    public void get() {
+        String tname = Thread.currentThread().getName();
+        System.out.printf("%s 取到第 %d 顆球\n", tname, amount);
+        amount--;
+    }
+}

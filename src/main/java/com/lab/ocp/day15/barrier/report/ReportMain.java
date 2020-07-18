a5dd12cf445e26b61b95aeeb363ec1c1c67790f0
MB-teacher <MB-teacher@192.168.1.109>
2020-07-18 15:45:26 +0800

    Java

--- src/main/java/com/lab/ocp/day15/barrier/report/ReportMain.java
+++ src/main/java/com/lab/ocp/day15/barrier/report/ReportMain.java
@@ -5,10 +5,13 @@ import java.util.concurrent.CyclicBarrier;
 public class ReportMain {
     public static void main(String[] args) {
         int parties = 3;
-        CyclicBarrier cb = new CyclicBarrier(3);
         Report report = new Report();
+        CyclicBarrier cb = new CyclicBarrier(parties, ()->{
+            System.out.println(report);
+        });
         new WriterAuthor(cb, report).start();
         new WriterContent(cb, report).start();
         new WriterPrice(cb, report).start();
+        
     }
 }

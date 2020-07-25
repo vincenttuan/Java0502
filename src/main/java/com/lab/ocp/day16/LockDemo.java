341ff4a20245db8f9b01c1c5edb27c49f4575f0c
MB-teacher <MB-teacher@192.168.1.109>
2020-07-25 13:45:36 +0800

    Java

--- src/main/java/com/lab/ocp/day16/LockDemo.java
+++ src/main/java/com/lab/ocp/day16/LockDemo.java
@@ -1,20 +1,26 @@
 package com.lab.ocp.day16;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
 class Lotto implements Runnable {
+    private String name;
     static ReentrantLock lock = new ReentrantLock();
+
+    public Lotto(String name) {
+        this.name = name;
+    }
     
     @Override
     public void run() {
-        String tName = Thread.currentThread().getName();
         try {
             lock.lock();
             IntStream.rangeClosed(1, 5)
-                    .forEach((i) -> System.out.printf("%s (%d) -> %d", 
-                                    tName, i, new Random().nextInt(100)));
+                    .forEach((i) -> System.out.printf("%s 第 %d 組號碼: %d\n", 
+                                    name, i, new Random().nextInt(100)));
         } catch (Exception e) {
         } finally {
             lock.unlock();
@@ -24,5 +30,10 @@ class Lotto implements Runnable {
 }
 
 public class LockDemo {
-    
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(new Lotto("Helen"));
+        service.execute(new Lotto("Bob"));
+        service.shutdown();
+    }
 }

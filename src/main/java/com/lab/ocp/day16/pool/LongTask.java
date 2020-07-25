25f29e58663ab8168a0e5125a082ff6ee56643a0
MB-teacher <MB-teacher@192.168.1.109>
2020-07-25 10:12:25 +0800

    Java

--- src/main/java/com/lab/ocp/day16/pool/LongTask.java
+++ src/main/java/com/lab/ocp/day16/pool/LongTask.java
@@ -9,7 +9,7 @@ public class LongTask implements Runnable {
         System.out.printf("長任務開始\n");
         int n = new Random().nextInt(9000);
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(new Random().nextInt(10));
         } catch (Exception e) {
         }
         n += 1000;

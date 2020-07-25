2586484f331c066eed1b173262eb5fbdbe956f26
MB-teacher <MB-teacher@192.168.1.109>
2020-07-25 16:24:50 +0800

    Java

--- src/main/java/com/lab/ocp/day17/RecursiveIteratorFolder.java
+++ src/main/java/com/lab/ocp/day17/RecursiveIteratorFolder.java
@@ -1,11 +1,12 @@
 package com.lab.ocp.day17;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class RecursiveIteratorFolder {
     public static void main(String[] args) {
         File path = new File("src\\main\\java\\com\\lab\\ocp\\day17");
-        list(path);
+        list2(path);
     }
     
     public static void list(File path) {
@@ -16,5 +17,13 @@ public class RecursiveIteratorFolder {
                 list(file);
             }
         }
+    }
+    
+    public static void list2(File path) {
+        File[] files = path.listFiles();
+        Arrays.stream(files).forEach((f) -> {
+            if (f.isDirectory()) list2(f);
+            System.out.println(f.getName());
+        });
     }
 }

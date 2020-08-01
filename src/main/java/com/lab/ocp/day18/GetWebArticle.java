44542335276fcc453d4faf9ecfe09d84e65ecfb7
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:44:11 +0800

    Java

--- src/main/java/com/lab/ocp/day18/GetWebArticle.java
+++ src/main/java/com/lab/ocp/day18/GetWebArticle.java
@@ -1,5 +1,7 @@
 package com.lab.ocp.day18;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.net.URL;
 import org.jsoup.Jsoup;
@@ -8,13 +10,22 @@ import org.jsoup.nodes.Document;
 public class GetWebArticle {
     public static void main(String[] args) {
         String path = "https://udn.com/news/story/12681/4746513?from=udn_ch2_menu_v2_main_index";
-        try(InputStream is = new URL(path).openStream()) {
+        try(InputStream is = new URL(path).openStream();) {
             byte[] data = new byte[is.available()];
             is.read(data);
             String html = new String(data, "utf-8");
             Document doc = Jsoup.parse(html);
-            System.out.println(doc.select("meta[property=\"og:title\"]"));
-            System.out.println(doc.select("meta[property=\"og:description\"]"));
+            String title = doc.select("meta[property=\"og:title\"]").attr("content");
+            String content = doc.select("meta[property=\"og:description\"]").attr("content");
+            System.out.println(title);
+            System.out.println(content);
+            File file = new File("src\\main\\java\\com\\lab\\ocp\\day18\\" + title + ".txt");
+            try(FileWriter fw = new FileWriter(file)) {
+                fw.write(content);
+                System.out.println("寫入完成");
+            } catch (Exception e) {
+            }
+            
         } catch (Exception e) {
         }
     }

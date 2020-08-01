217725a08ef2e51be2b4ef2a9501a46d09b52259
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:24:23 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day18/GetWebArticle.java
@@ -0,0 +1,7 @@
+package com.lab.ocp.day18;
+
+public class GetWebArticle {
+    public static void main(String[] args) {
+        String path = "https://udn.com/news/story/12681/4746513?from=udn_ch2_menu_v2_main_index";
+    }
+}

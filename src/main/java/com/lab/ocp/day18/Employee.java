58cce893f6ca92287adfdf4687c775311a89e2fc
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 12:05:44 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day18/Employee.java
@@ -0,0 +1,30 @@
+package com.lab.ocp.day18;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private String name;
+    private int salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + ", salary=" + salary + '}';
+    }
+    
+}

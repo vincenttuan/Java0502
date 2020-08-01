9eb788e0ec427e9b3d7f755417461d16a6764e62
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:40:21 +0800

    Java

--- src/main/java/com/lab/ocp/day18/GetWebArticle.java
+++ src/main/java/com/lab/ocp/day18/GetWebArticle.java
@@ -2,15 +2,19 @@ package com.lab.ocp.day18;
 
 import java.io.InputStream;
 import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class GetWebArticle {
     public static void main(String[] args) {
         String path = "https://udn.com/news/story/12681/4746513?from=udn_ch2_menu_v2_main_index";
         try(InputStream is = new URL(path).openStream()) {
-            
             byte[] data = new byte[is.available()];
             is.read(data);
-            System.out.print(new String(data, "utf-8"));
+            String html = new String(data, "utf-8");
+            Document doc = Jsoup.parse(html);
+            System.out.println(doc.select("meta[property=\"og:title\"]"));
+            System.out.println(doc.select("meta[property=\"og:description\"]"));
         } catch (Exception e) {
         }
     }

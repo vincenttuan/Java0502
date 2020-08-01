37fefe937f7e38ab7561d366c52c6cacca02188b
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:14:16 +0800

    Java

--- src/main/java/com/lab/ocp/day18/GetWebImage.java
+++ src/main/java/com/lab/ocp/day18/GetWebImage.java
@@ -1,6 +1,9 @@
 package com.lab.ocp.day18;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 public class GetWebImage {
     public static void main(String[] args) {
@@ -8,5 +11,17 @@ public class GetWebImage {
         String path = "https://uc.udn.com.tw/photo/2019/10/15/99/6943615.jpg";
         // 寫入檔案
         File file = new File("src\\main\\java\\com\\lab\\ocp\\day18\\car.jpg");
+        // InputStream 讀取串流
+        // OutputStream 寫入串流, FileOutputStream 寫入串流到指定檔案
+        try(InputStream is = new URL(path).openStream();
+            FileOutputStream fos = new FileOutputStream(file);) {
+            int data;
+            while ((data = is.read()) != -1) {                
+                fos.write(data);
+            }
+            System.out.println("寫入成功");
+        } catch (Exception e) {
+        }
+
     }
 }

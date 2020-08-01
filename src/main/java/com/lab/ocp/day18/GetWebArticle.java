e0cb926b9c437681769bf12b6a0a4f05b3e8dd68
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:35:16 +0800

    Java

--- src/main/java/com/lab/ocp/day18/GetWebArticle.java
+++ src/main/java/com/lab/ocp/day18/GetWebArticle.java
@@ -1,7 +1,17 @@
 package com.lab.ocp.day18;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class GetWebArticle {
     public static void main(String[] args) {
         String path = "https://udn.com/news/story/12681/4746513?from=udn_ch2_menu_v2_main_index";
+        try(InputStream is = new URL(path).openStream()) {
+            
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            System.out.print(new String(data, "utf-8"));
+        } catch (Exception e) {
+        }
     }
 }

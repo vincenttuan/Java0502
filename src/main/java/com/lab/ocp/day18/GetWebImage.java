05181d18d9b5532ac194673bb665884cb3ff1094
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 10:09:24 +0800

    Java

---
+++ src/main/java/com/lab/ocp/day18/GetWebImage.java
@@ -0,0 +1,12 @@
+package com.lab.ocp.day18;
+
+import java.io.File;
+
+public class GetWebImage {
+    public static void main(String[] args) {
+        // 網路來源
+        String path = "https://uc.udn.com.tw/photo/2019/10/15/99/6943615.jpg";
+        // 寫入檔案
+        File file = new File("src\\main\\java\\com\\lab\\ocp\\day18\\car.jpg");
+    }
+}

dd84cb817235d9e45e1a229673ad9cbcf4b6b669
MB-teacher <MB-teacher@192.168.1.109>
2020-08-01 11:06:42 +0800

    Java

--- src/main/java/com/lab/ocp/day18/GetWebImage.java
+++ src/main/java/com/lab/ocp/day18/GetWebImage.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class GetWebImage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 網路來源
-        String path = "https://previews.123rf.com/images/popcar/popcar1511/popcar151100028/48549386-retro-car-.jpg";
+        String path = "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg";
         // 寫入檔案
         File file = new File("src\\main\\java\\com\\lab\\ocp\\day18\\car.jpg");
         // InputStream 讀取串流
         // OutputStream 寫入串流, FileOutputStream 寫入串流到指定檔案
-        try(InputStream is = new URL(path).openStream();
+        URLConnection conn = new URL(path).openConnection();
+        conn.setRequestProperty("USER-AGENT", "我不是機器人");
+        try(InputStream is = conn.getInputStream();
             FileOutputStream fos = new FileOutputStream(file);) {
             int data;
             while ((data = is.read()) != -1) {                
@@ -21,6 +24,7 @@ public class GetWebImage {
             }
             System.out.println("寫入成功");
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
